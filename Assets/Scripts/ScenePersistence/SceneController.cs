1ddb7f55ba03172829a19799c4b76bda7fb3e690
HubertBoomStick <wrighttre55@gmail.com>
2026-05-13 14:39:19 -0400

    man menu with options and quit

--- Assets/Scripts/ScenePersistence/SceneController.cs
+++ Assets/Scripts/ScenePersistence/SceneController.cs
@@ -5,10 +5,25 @@ public class SceneChanger : MonoBehaviour
 {
 
     public string SceneToLoad;
+    public bool isPLaying;
 
     public void changeSceneNow()
     {
         SceneManager.LoadScene(SceneToLoad);
     }
+
+    public void EndGame()
+    {
+        Debug.Log("Exiting Game!!!!!");
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+            Application.Quit();
+
+        #endif
+    }
+
 
 }
