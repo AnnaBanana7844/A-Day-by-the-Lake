b8948231ec42f3af3f2446b9de7ba4637201ed0c
HubertBoomStick <wrighttre55@gmail.com>
2026-05-13 13:57:26 -0400

    main menu

--- Assets/Scripts/ScenePersistence/SceneController.cs
+++ Assets/Scripts/ScenePersistence/SceneController.cs
@@ -5,10 +5,29 @@ public class SceneChanger : MonoBehaviour
 {
 
     public string SceneToLoad;
+    public bool isPlaying;
 
     public void changeSceneNow()
     {
         SceneManager.LoadScene(SceneToLoad);
     }
 
+
+    public void EndGame()
+    {
+        Debug.Log("Game is Closing");
+
+        #if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+
+            Application.Quit(); 
+
+        #endif
+
+        
+        
+    }
 }
