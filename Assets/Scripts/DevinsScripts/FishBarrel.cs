99dbbdd5f50117766376d9caaeefcc67a68ea5ea
Devin Miller <AEMiller@student.fullsail.edu>
2026-05-24 22:05:53 -0400

    RICKY HAS SPAWNED
    added bear and player plus bus to play scene :D

--- Assets/Scripts/DevinsScripts/FishBarrel.cs
+++ Assets/Scripts/DevinsScripts/FishBarrel.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FishBarrel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    int fishCount;
+    [SerializeField] SphereCollider interact;
+    [SerializeField] Text fishDisplay;
+    
+    void Start()// Start is called once before the first execution of Update after the MonoBehaviour is created
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void Update()// Update is called once per frame
     {
         
     }
